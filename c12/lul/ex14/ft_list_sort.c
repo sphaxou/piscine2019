7ce43995871f84286766f9746d6125676c4c1afd
Victor GALLOIS-WONG <vgallois@e2r4p17.42.fr>
2019-06-21 16:45:41 +0200

    sort

--- c12/lul/ex14/ft_list_sort.c
+++ c12/lul/ex14/ft_list_sort.c
@@ -6,36 +6,44 @@
 /*   By: vgallois <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 23:11:37 by vgallois          #+#    #+#             */
-/*   Updated: 2019/06/21 03:57:04 by vgallois         ###   ########.fr       */
+/*   Updated: 2019/06/21 16:45:31 by vgallois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-void	ft_list_swap(t_list **l)
+void	ft_list_insert_sort(t_list **list, t_list *elem,
+		int (*cmp)(void*, void*))
 {
 	t_list	*tmp;
 
-	tmp = (*l)->next->next;
-	(*l)->next->next = (*l);
-	(*l)->next = tmp;
-
+	tmp = *list;
+	if (cmp(elem->data, tmp->data) < 0)
+	{
+		elem->next = tmp->next;
+		tmp->next = elem;
+		return ;
+	}
+	while (tmp->next && cmp(elem->data, tmp->data))
+		tmp = tmp->next;
+	elem->next = tmp->next;
+	tmp->next = elem;
 }
 
-void	ft_list_sort(t_list **begin_list, int (*cmp)(void*, void*))
+void	ft_list_sort(t_list **start, int (*cmp)(void*, void*))
 {
-	t_list	*cur;
+	t_list	*list;
+	t_list	*tmp;
+	t_list	*tmp2;
 
-	cur = *begin_list;
-	while (cur && cur->next)
+	list = *start;
+	tmp = list->next;
+	list->next = NULL;
+	while(tmp)
 	{
-		printf("%s\n", cur->data);
-		if (cmp(cur->data, cur->next->data) < 0)
-		{
-			ft_list_swap(&cur);
-			cur = *begin_list;
-		}
-		else
-			cur = cur->next;
+		tmp2 = tmp->next;
+		ft_list_insert_sort(&list, tmp, cmp);
+		tmp = tmp2;
 	}
+
 }
