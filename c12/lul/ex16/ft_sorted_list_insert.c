a7e47bacbc6a478a4d027d4a5bbfcf7ee219d052
Victor GALLOIS-WONG <vgallois@e2r4p17.42.fr>
2019-06-21 16:45:55 +0200

    ex16

---
+++ c12/lul/ex16/ft_sorted_list_insert.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sorted_list_insert.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgallois <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/21 16:20:08 by vgallois          #+#    #+#             */
+/*   Updated: 2019/06/21 16:34:27 by vgallois         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void ft_sorted_list_insert(t_list **start, void *data, int (*cmp)(void*, void*))
+{
+	t_list	*new;
+	t_list	*list;
+
+	new = ft_create_elem(data);
+	list = (*start);
+	if (cmp(new->data, list->data) <= 0)
+	{
+		new->next = list;
+		*start = new;
+		return ;
+	}
+	while (list && cmp(new->data, list->data) > 0)
+		list = list->next;
+	new->next = list->next;
+	list->next = new;
+}		
